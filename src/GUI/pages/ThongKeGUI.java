199b39bdfd43d50ca81f231ddf71e8b5e9dc24e1
QuoccViett <3123560093@sv.sgu.edu.vn>
2025-04-30 23:22:28 +0700

    Update ThongKeGUI.java

--- src/GUI/pages/ThongKeGUI.java
+++ src/GUI/pages/ThongKeGUI.java
@@ -64,206 +64,9 @@ public class ThongKeGUI extends JPanel {
 
     public ThongKeGUI(TopNav topNav) {
         this.setLayout(new BorderLayout());
-        // initComponent(topNav);
-        // initComponent();
         itemButtons = new ArrayList<>();
         DisplayDashboardCards();
     }
-
-    // public void initComponent(){
-    // this.setLayout(new BorderLayout());
-    // Font font = new Font("Arial", Font.BOLD, 20);
-    // // Header
-    // JPanel jPanelHeader = new JPanel();
-    // JLabel lbHeader = new JLabel("TONG HOP - THONG KE", SwingConstants.LEFT);
-    // lbHeader.setFont(new Font("Arial", Font.BOLD, 40));
-    // lbHeader.setForeground(new Color(0, 153, 153));
-    // jPanelHeader.add(lbHeader);
-
-    // JPanel jPanelCenter = new JPanel();
-    // jPanelCenter.setLayout(new BorderLayout());
-
-    // JPanel jPanelCenterNorth = new JPanel();
-    // jPanelCenterNorth.setLayout(new GridLayout(1, 2));
-
-    // JPanel jPanelDoanhThu = new JPanel();
-    // jPanelDoanhThu.setLayout(new BorderLayout());
-    // jPanelDoanhThu.setBackground(new Color(36, 167, 224));
-    // jPanelDoanhThu.setPreferredSize(new Dimension(200, 400));
-
-    // JLabel lbDoanhThuHomNay = new JLabel("DOANH THU", SwingConstants.CENTER);
-    // lbDoanhThuHomNay.setFont(font);
-    // lbDoanhThuHomNay.setForeground(Color.WHITE);
-
-    // JLabel lbValueDoanhThu = new JLabel("0", SwingConstants.CENTER);
-    // lbValueDoanhThu.setFont(font);
-    // lbValueDoanhThu.setForeground(Color.BLACK);
-
-    // jPanelDoanhThu.add(lbDoanhThuHomNay, BorderLayout.NORTH);
-    // jPanelDoanhThu.add(lbValueDoanhThu, BorderLayout.CENTER);
-
-    // JPanel jPanelLoiNhuan = new JPanel();
-    // jPanelLoiNhuan.setLayout(new BorderLayout());
-    // jPanelLoiNhuan.setBackground(new Color(36, 167, 224));
-    // jPanelLoiNhuan.setPreferredSize(new Dimension(200, 400));
-
-    // JLabel lbLoiNhuan = new JLabel("LOI NHUAN", SwingConstants.CENTER);
-    // lbLoiNhuan.setFont(font);
-    // lbLoiNhuan.setForeground(Color.WHITE);
-
-    // JLabel lbValueLoiNhuan = new JLabel("0", SwingConstants.CENTER);
-    // lbValueLoiNhuan.setFont(font);
-    // lbValueLoiNhuan.setForeground(Color.BLACK);
-
-    // JPanel jPanelFillter = new JPanel(new GridLayout(2, 3, 5, 5));
-    // String[] time = {"Hom nay", "Tuan nay", "Thang Nay"};
-    // JComboBox<String> comboBox = new JComboBox<>(time);
-
-    // JTextField startDay = new JTextField();
-    // JTextField endDay = new JTextField();
-    // JButton btnFillter = new JButton("Loc");
-    // btnFillter.setBackground(Color.BLACK);
-    // btnFillter.setForeground(Color.WHITE);
-
-    // JLabel lbStartDay = new JLabel("Thoi gian bat dau: ");
-    // lbStartDay.setForeground(Color.GRAY);
-    // JLabel lbEndDay = new JLabel("Thoi gian ket thuc: ");
-    // lbEndDay.setForeground(Color.GRAY);
-
-    // jPanelFillter.add(comboBox);
-    // jPanelFillter.add(new JLabel(""));
-    // jPanelFillter.add(new JLabel(""));
-    // jPanelFillter.add(lbStartDay);
-    // jPanelFillter.add(lbEndDay);
-    // jPanelFillter.add(new JLabel(""));
-    // jPanelFillter.add(startDay);
-    // jPanelFillter.add(endDay);
-    // jPanelFillter.add(btnFillter);
-
-    // jPanelLoiNhuan.add(lbLoiNhuan, BorderLayout.NORTH);
-    // jPanelLoiNhuan.add(lbValueLoiNhuan, BorderLayout.CENTER);
-    // jPanelLoiNhuan.add(jPanelFillter, BorderLayout.SOUTH);
-
-    // jPanelCenterNorth.add(jPanelDoanhThu);
-    // jPanelCenterNorth.add(jPanelLoiNhuan);
-
-    // jPanelCenter.add(jPanelCenterNorth, BorderLayout.NORTH);
-
-    // this.add(jPanelHeader, BorderLayout.NORTH);
-    // this.add(jPanelCenter, BorderLayout.CENTER);
-
-    // }
-
-    // this.setLayout(new BorderLayout());
-
-    // // Tiêu đề và khu vực số liệu
-    // JPanel topPanel = new JPanel(new GridLayout(1, 2, 20, 10));
-    // topPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-
-    // // Doanh thu hôm nay
-    // JPanel revenuePanel = new JPanel(new BorderLayout());
-    // revenuePanel.setBackground(new Color(0, 153, 204));
-    // revenuePanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-
-    // JLabel revenueLabel = new JLabel("DOANH THU HÔM NAY", SwingConstants.CENTER);
-    // revenueLabel.setForeground(Color.WHITE);
-    // revenueLabel.setFont(new Font("SansSerif", Font.BOLD, 16));
-    // JLabel revenueValue = new JLabel("564.000 đ", SwingConstants.CENTER);
-    // revenueValue.setForeground(Color.WHITE);
-    // revenueValue.setFont(new Font("SansSerif", Font.BOLD, 24));
-
-    // revenuePanel.add(revenueLabel, BorderLayout.NORTH);
-    // revenuePanel.add(revenueValue, BorderLayout.CENTER);
-
-    // // Lợi nhuận hôm nay
-    // JPanel profitPanel = new JPanel(new BorderLayout());
-    // profitPanel.setBackground(new Color(0, 128, 128));
-    // profitPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-
-    // JLabel profitLabel = new JLabel("Lợi Nhuận", SwingConstants.CENTER);
-    // profitLabel.setForeground(Color.WHITE);
-    // profitLabel.setFont(new Font("SansSerif", Font.BOLD, 16));
-    // JLabel profitValue = new JLabel("190.500 đ", SwingConstants.CENTER);
-    // profitValue.setForeground(Color.WHITE);
-    // profitValue.setFont(new Font("SansSerif", Font.BOLD, 24));
-
-    // profitPanel.add(profitLabel, BorderLayout.NORTH);
-    // profitPanel.add(profitValue, BorderLayout.CENTER);
-
-    // topPanel.add(revenuePanel);
-    // topPanel.add(profitPanel);
-
-    // // Biểu đồ doanh thu (giả lập)
-    // // JPanel chartPanel = new JPanel() {
-    // // @Override
-    // // protected void paintComponent(Graphics g) {
-    // // super.paintComponent(g);
-    // // // Vẽ trục
-    // // g.setColor(Color.LIGHT_GRAY);
-    // // for (int i = 0; i <= 6; i++) {
-    // // g.drawLine(50, 300 - i * 40, 500, 300 - i * 40);
-    // // }
-    // // // Vẽ cột dữ liệu
-    // // g.setColor(Color.RED);
-    // // g.fillRect(100, 290, 50, 10); // ngày 08
-    // // g.fillRect(200, 250, 50, 50); // ngày 09
-    // // g.fillRect(300, 80, 50, 220); // ngày 10
-    // // }
-    // // };
-    // // chartPanel.setPreferredSize(new Dimension(600, 320));
-    // // chartPanel.setBorder(BorderFactory.createTitledBorder("BIỂU ĐỒ THỐNG KÊ
-    // DOANH THU"));
-
-    // // Thêm các thành phần vào panel chính
-    // add(topPanel, BorderLayout.NORTH);
-    // // add(chartPanel, BorderLayout.CENTER);
-    // }
-
-    // Tạo JFrame để chạy thử
-    // rpublic static void main(String[] args) {
-    // JFrame frame = new JFrame("TỔNG HỢP - THỐNG KÊ");
-    // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    // frame.setSize(700, 600);
-    // fame.setLocationRelativeTo(null);
-    // frame.setContentPane(new ThongKePanel());
-    // frame.setVisible(true);
-    // }
-    // }
-
-    // private void initComponent(TopNav topNav) {
-    // this.topNav = topNav;
-    // String[] itemFindFor = { "Tất Cả" };
-    // topNav.setItemComboBox(itemFindFor);
-
-    // // Bottom Panel
-    // pnlBot = new JPanel(new BorderLayout());
-    // pnlBot.setPreferredSize(new Dimension(0, 500));
-
-    // // Create JTable
-    // tbl = new JTable();
-    // tbl.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-    // tbl.setRowHeight(35);
-    // tbl.setFocusable(false);
-
-    // // Style the table header
-    // JTableHeader header = tbl.getTableHeader();
-    // header.setPreferredSize(new Dimension(0, 40));
-    // header.setBackground(new Color(100, 149, 237)); // Cornflower Blue
-    // header.setForeground(Color.WHITE);
-    // header.setFont(new Font("Segoe UI", Font.BOLD, 14));
-
-    // // Add JScrollPane containing the table
-    // JScrollPane scrollPane = new JScrollPane(tbl);
-    // scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-    // scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-
-    // pnlBot.setBorder(new EmptyBorder(10, 15, 10, 15));
-    // pnlBot.add(scrollPane, BorderLayout.CENTER);
-
-    // // Set layout for TaiKhoanGUI
-    // this.setLayout(new BorderLayout());
-    // // this.add(pnlBot, BorderLayout.CENTER);
-    // }
 
     public void DisplayDashboardCards() {
         ThongKeDao thongKeDao = new ThongKeDao();
@@ -282,7 +85,7 @@ public class ThongKeGUI extends JPanel {
 
         JPanel jPanel = new JPanel();
         jPanel.setLayout(new BorderLayout());
-        jPanel.setBackground(new java.awt.Color(250, 250, 250));// Màu nền nhạt toàn giao diện
+        jPanel.setBackground(new java.awt.Color(250, 250, 250));
 
         JLabel lbTitle = new JLabel("TỔNG HỢP - THỐNG KÊ", JLabel.CENTER);
         lbTitle.setFont(new Font("Arial", Font.BOLD, 40));
@@ -315,7 +118,7 @@ public class ThongKeGUI extends JPanel {
                 ThongKeGUI.this.remove(chartPanelCurrent);
                 ThongKeGUI.this.add(chartPanelDoanhThu, BorderLayout.CENTER);
                 chartPanelCurrent = chartPanelDoanhThu;
-                ThongKeGUI.this.revalidate(); // Cập nhật lại bố cục giao diện
+                ThongKeGUI.this.revalidate();
                 ThongKeGUI.this.repaint();
             }
         });
@@ -326,7 +129,7 @@ public class ThongKeGUI extends JPanel {
                 ThongKeGUI.this.remove(chartPanelCurrent);
                 ThongKeGUI.this.add(chartPanelKhachHang, BorderLayout.CENTER);
                 chartPanelCurrent = chartPanelKhachHang;
-                ThongKeGUI.this.revalidate(); // Cập nhật lại bố cục giao diện
+                ThongKeGUI.this.revalidate();
                 ThongKeGUI.this.repaint();
             }
         });
@@ -337,7 +140,7 @@ public class ThongKeGUI extends JPanel {
                 ThongKeGUI.this.remove(chartPanelCurrent);
                 ThongKeGUI.this.add(chartPanelSanPham, BorderLayout.CENTER);
                 chartPanelCurrent = chartPanelSanPham;
-                ThongKeGUI.this.revalidate(); // Cập nhật lại bố cục giao diện
+                ThongKeGUI.this.revalidate();
                 ThongKeGUI.this.repaint();
             }
         });
@@ -348,7 +151,7 @@ public class ThongKeGUI extends JPanel {
                 ThongKeGUI.this.remove(chartPanelCurrent);
                 ThongKeGUI.this.add(chartPanelNhanVien, BorderLayout.CENTER);
                 chartPanelCurrent = chartPanelNhanVien;
-                ThongKeGUI.this.revalidate(); // Cập nhật lại bố cục giao diện
+                ThongKeGUI.this.revalidate();
                 ThongKeGUI.this.repaint();
             }
         });
@@ -373,12 +176,9 @@ public class ThongKeGUI extends JPanel {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                // LocalDate start = (LocalDate) startDatePicker.getModel().getValue();
-                // LocalDate end = (LocalDate) endDatePicker.getModel().getValue();
                 java.util.Date startUtilDate = (java.util.Date) startDatePicker.getModel().getValue();
                 java.util.Date endUtilDate = (java.util.Date) endDatePicker.getModel().getValue();
-
-                // Chuyển đổi từ java.util.Date thành LocalDate
+                
                 LocalDate start = startUtilDate.toInstant().atZone(java.time.ZoneId.systemDefault()).toLocalDate();
                 LocalDate end = endUtilDate.toInstant().atZone(java.time.ZoneId.systemDefault()).toLocalDate();
                 ThongKeDTO thongKeDTO = new ThongKeDTO(start, end);
@@ -456,30 +256,24 @@ public class ThongKeGUI extends JPanel {
 
     public JPanel createDashboardCards(String title, int value) {
         JPanel jPanelDashboardCard = new JPanel();
-        // jPanelDashboardCard.setSize(150, 150);
         jPanelDashboardCard.setLayout(new BorderLayout());
         jPanelDashboardCard.setBackground(new Color(100, 149, 237));
         jPanelDashboardCard.setForeground(Color.WHITE);
         jPanelDashboardCard.setPreferredSize(new Dimension(120, 120));
-        // jPanelDashboardCard.setBorder(BorderFactory.createCompoundBorder(
-        // BorderFactory.createEmptyBorder(10, 10, 10, 10),
-        // BorderFactory.createLineBorder(new Color(255, 70, 130, 30), 5)));
+
         jPanelDashboardCard.setOpaque(true);
         JLabel lbTitle = new JLabel(title, SwingConstants.CENTER);
         lbTitle.setFont(new Font("SansSerif", Font.BOLD, 16));
         lbTitle.setForeground(Color.WHITE);
-        // lbTitle.setBackground(Color.WHITE);
 
         JLabel lbValue = new JLabel(String.valueOf(value), SwingConstants.CENTER);
         lbValue.setFont(new Font("SansSerif", Font.BOLD, 28));
         lbValue.setForeground(Color.WHITE);
-        // lbValue.setBackground(Color.WHITE);
 
         JButton btnDetail = new JButton();
         btnDetail.setText("Xem Chi Tiết");
         btnDetail.setFont(new Font("SansSerif", Font.BOLD, 16));
         btnDetail.setForeground(Color.BLACK);
-        // btnDetail.setBackground(Color.WHITE);
         btnDetail.setFocusPainted(false);
         btnDetail.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
         btnDetail.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
@@ -493,8 +287,7 @@ public class ThongKeGUI extends JPanel {
         return jPanelDashboardCard;
     }
 
-    // Khách Hàng Mua Hàng Nhiều Nhất
-    // Column
+
     public ChartPanel createChartKhachHang() {
         ThongKeDao thongKeDao = new ThongKeDao();
         List<ThongKeDTO> list = thongKeDao.ThongKeKhachHang();
@@ -503,8 +296,8 @@ public class ThongKeGUI extends JPanel {
             dataset.addValue(thongKeDTO.getSoluong(), "Tống Giá Trị Mua Hàng", thongKeDTO.getTen());
         }
         JFreeChart chart = ChartFactory.createBarChart3D("Thống Kê Khách Hàng Mua Nhiều Nhất", "Tên Khách Hàng",
-                "Tống Giá Trị Mua Hàng", dataset, PlotOrientation.VERTICAL, // Chiều biểu đồ
-                true, // Hiển thị chú thích
+                "Tống Giá Trị Mua Hàng", dataset, PlotOrientation.VERTICAL,
+                true, 
                 true,
                 false);
 
@@ -534,8 +327,8 @@ public class ThongKeGUI extends JPanel {
             dataset.addValue(thongKeDTO.getSoluong(), "Tống Giá Trị Mua Hàng", thongKeDTO.getTen());
         }
         JFreeChart chart = ChartFactory.createBarChart3D("Thống Kê Khách Hàng Mua Nhiều Nhất", "Tên Khách Hàng",
-                "Tống Giá Trị Mua Hàng", dataset, PlotOrientation.VERTICAL, // Chiều biểu đồ
-                true, // Hiển thị chú thích
+                "Tống Giá Trị Mua Hàng", dataset, PlotOrientation.VERTICAL, 
+                true, 
                 true,
                 false);
                 CategoryPlot plot = chart.getCategoryPlot();
@@ -555,8 +348,6 @@ public class ThongKeGUI extends JPanel {
         return chartPanel;
     }
 
-    // Sản Phẩm Bán Chạy Nhất
-    // Pie
     public ChartPanel createChartSanPham() {
         ThongKeDao thongKeDao = new ThongKeDao();
         List<ThongKeDTO> list = thongKeDao.ThongKeSanPham();
@@ -619,7 +410,6 @@ public class ThongKeGUI extends JPanel {
         return chartPanel;
     }
 
-    // Nhân Viên Bán Được Hàng Nhiều Nhất
     public ChartPanel createChartNhanVien() {
         ThongKeDao thongKeDao = new ThongKeDao();
         List<ThongKeDTO> thongKeDTO = thongKeDao.ThongKeNhanVien();
@@ -672,7 +462,6 @@ public class ThongKeGUI extends JPanel {
         return chartPanel;
     }
 
-    // Tổng Doanh Thu Theo Ngày ( Tháng, Năm )
     public ChartPanel createChartDoanhThu() {
         ThongKeDao thongKeDao = new ThongKeDao();
         List<ThongKeDTO> thongKeDTO = thongKeDao.ThongKeDoanhThu();
